c7224169dfe99dc10429adfb98f3b0dacbbe8c01
Zanma <zainurimahmud32@gmail.com>
2021-09-27 10:08:53 +0700

    add model2 (detail)

---
+++ app/Views/komik/detail.php
@@ -0,0 +1,31 @@
+<?= $this->extend('layout/template'); ?>
+
+
+<?= $this->section('content'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1 class="mt-2">Detail Komik</h1>
+            <div class="card mb-3" style="max-width: 540px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="/img/<?= $komik['sampul']; ?>" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title"><?= $komik['judul']; ?></h5>
+                            <p class="card-text"><b>Penulis : </b><?= $komik['penulis']; ?></p>
+                            <p class="card-text"><small class="text-muted"><b>Penerbit : </b><?= $komik['penerbit']; ?></small></p>
+
+                            <a class="btn btn-light border border-dark" href="">Edit</a>
+                            <a class="btn btn-danger" href="">Delete</a>
+                            <br><br>
+                            <a href="/komik">Kembali ke Daftar Komik</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?= $this->endSection(); ?>
